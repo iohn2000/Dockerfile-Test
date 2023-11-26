da9fc8d264d6dfcc673220ac81586fc851aed8e1
FleckJ <johannes.fleck@k-business.com>
2023-11-26 19:41:14 +0100

    initial commit with working example

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+string fname = "mountpoint/bind.txt";
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("trying to write to a file bind.txt");
+ File.AppendAllText(fname, $"hello:{DateTime.Now + Environment.NewLine}");
+
+Console.WriteLine("trying to read it now");
+var content = File.ReadAllText(fname);
+Console.WriteLine($"content is:{Environment.NewLine + content + Environment.NewLine}");
+Console.WriteLine("end");
